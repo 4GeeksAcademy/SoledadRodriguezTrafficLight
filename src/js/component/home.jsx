1eb33f23b269e64372b0b01a5dad081c33d248c0
soledadrodriguez <68907554+soledadrodriguez@users.noreply.github.com>
2024-10-16 23:45:12 +0000

    [use state para cambiar la luz por interacción]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selected, setSelected] = useState("red")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className=" container trafficLight ">
+			<div 
+				className= {`red ${selected == "red" ? "lightOn" : ""}`}
+				onClick={()=>setSelected("red")}
+			></div>
+			<div className= {`yellow ${selected == "yellow" ? "lightOn" : ""}`}
+				onClick={()=>setSelected("yellow")}></div>
+			<div className={`green ${selected == "green" ? "lightOn" : ""}`}
+				onClick={()=>setSelected("green")} ></div>
+		</div >
+	
 	);
 };
 
